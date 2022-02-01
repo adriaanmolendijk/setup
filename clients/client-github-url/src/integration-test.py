97eba22bc533e524aaf807cadda544d285e84791
Adriaan Molendijk <a.l.molendijk@outlook.com>
2022-02-01 19:05:48 +0100

    Revert "Removed GitHub client"
    This reverts commit 51f578e65f3f930761d0e198b09023bd0bfda09e.

---
+++ clients/client-github-url/src/integration-test.py
@@ -0,0 +1,8 @@
+from src.app import get_github_url
+
+
+def test_github_url():
+    root_address = 'github.com/adriaanmolendijk/setup'
+    assert get_github_url('') == root_address
+    assert get_github_url('b') == root_address + '/branches'
+    assert get_github_url('pr') == root_address + '/pulls'
