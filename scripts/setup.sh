b17f31722f2314ef593cbb5703088bcf8d2f5c37
Adriaan Molendijk <65175073+adriaanmolendijk@users.noreply.github.com>
2022-02-19 09:28:04 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,14 @@
-# ~/.zshrc
-cat home/.zshrc > ~/.zshrc
-echo "" >> ~/.zshrc
+# home folder
+cp -r home/. ~/
 
-cp home/.vimrc ~/.vimrc
-cp home/.gitignore_global ~/.gitignore_global
+# git setup
+git config --global core.editor vim
+git config --global core.excludesfile ~/.gitignore_global
+
+git config --global alias.b               'branch'
+git config --global alias.co              'checkout'
+git config --global alias.cm              'commit -m'
+git config --global alias.p               'push'
+git config --global alias.prune-branches '!git fetch -p && git branch -vv | grep '"'"': gone]'"'"' | awk '"'"'{print $1}'"'"' | xargs -r git branch -D'
+git config --global alias.pl              'pull'
+git config --global alias.st              'status'
