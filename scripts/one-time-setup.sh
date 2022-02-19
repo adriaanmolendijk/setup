2cc7f7e1cd918dd32fff18b18596e7e048ebf443
Adriaan Molendijk <65175073+adriaanmolendijk@users.noreply.github.com>
2022-02-19 08:09:44 +0100

    Create one-time-setup.sh

---
+++ scripts/one-time-setup.sh
@@ -0,0 +1,30 @@
+### @adriaanmolendijk ###
+
+# brew update
+brew update
+
+# general tools
+brew install wget
+brew install curl
+brew install tree
+brew install jq
+brew install git
+
+brew install --cask sublime-text
+brew install --cask google-chrome
+
+# development
+# install Docker
+brew install kubectl
+brew install lens
+brew install helm
+
+brew install make
+brew install mvn
+brew install terraform
+brew install terragrunt
+brew install postman
+
+brew install --cask intellij-idea-ce
+
+### @adriaanmolendijk ###
