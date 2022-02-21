47ed494dec0d9e2470fb346ab81245c025a8bb03
Adriaan Molendijk <adriaan.molendijk@tomtom.com>
2022-02-21 07:51:56 +0100

    Reformatted macOS install file

--- macOS/install.sh
+++ macOS/install.sh
@@ -2,17 +2,20 @@
 brew update
 
 # general tools
-brew install wget
-brew install curl
 brew install tree
 brew install jq
-brew install git
-
 brew install --cask google-chrome
 brew install --cask sublime-text
 
+# networking
+brew install wget
+brew install curl
+brew install postman
+
 # development
-# install Docker
+brew install git
+
+brew install --cask docker
 brew install kubectl
 brew install lens
 brew install helm
@@ -21,7 +24,6 @@ brew install make
 brew install mvn
 brew install terraform
 brew install terragrunt
-brew install postman
 
 brew install --cask intellij-idea-ce
 brew install --cask pycharm-ce
