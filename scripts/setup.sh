0fc20f8963db2475d564ca89c1424d404970138f
Adriaan Molendijk <adriaan.molendijk@tomtom.com>
2022-03-10 18:28:59 +0100

    Reverted aliases again

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,10 +8,4 @@ cp tools/tool-github-url/src/app.py ~/tool_github_url.py
 git config --global core.editor vim
 git config --global core.excludesfile ~/.gitignore_global
 
-git config --global alias.b                       'branch'
-git config --global alias.co                      'checkout'
-git config --global alias.cm                      'commit -m'
-git config --global alias.p                       'push'
 git config --global alias.delete-remote-branches  '!git fetch -p && git branch -vv | grep '"'"': gone]'"'"' | awk '"'"'{print $1}'"'"' | xargs -r git branch -D'
-git config --global alias.pl                      'pull'
-git config --global alias.st                      'status'
