6850887308d28db4e8543b9f2414455109b0f655
Adriaan Molendijk <adriaan.molendijk@tomtom.com>
2022-03-20 16:14:52 +0100

    Moved github tool to specific dir

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,6 @@
 # home folder
 cp -r home/. ~
 
-# tools
-cp tools/tool-github-url/src/app.py ~/tool_github_url.py
-
 # git setup
 git config --global core.editor vim
 git config --global core.excludesfile ~/.gitignore_global
