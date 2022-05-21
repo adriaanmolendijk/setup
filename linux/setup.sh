c5448ce9768618619125a00da4c6eedaa5e5ac82
Adriaan Molendijk <adriaan.molendijk@tomtom.com>
2022-05-21 11:58:20 +0200

    Removed old alias

--- linux/setup.sh
+++ linux/setup.sh
@@ -9,4 +9,3 @@ cp home/.tmux.conf home/.vimrc ~
 # git config
 git config --global core.editor "vim"
 git config --global core.excludesfile "~/.config/adriaan/.gitignore_global"
-git config --global alias.delete-remote-branches '!git fetch -p && git branch -vv | grep ": gone]" | awk "{print $1}" | xargs -r git branch -D'
